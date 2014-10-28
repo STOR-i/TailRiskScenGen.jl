947483ad1ee5510a0fab50e65f000ae742f38bbd
Jamie Fairbrother <jfairbrother87@hotmail.com>
2014-10-28 16:55:44 +0000

    - Removed redundent macro from aggregate_scenarios function - Added aggregate_scenarios example to runtests.jl

--- src/ScenarioReduction.jl
+++ src/ScenarioReduction.jl
@@ -1,9 +1,8 @@
 using Cpp
 using Distributions
 
-dir = dirname(@__FILE__())
-lib =  joinpath(dir, "../bin/libScenGen.so")
-lib = normpath(lib)
+lib_path =  joinpath(dirname(@__FILE__()), "../bin/libScenGen.so")
+const lib = normpath(lib_path)
 
 function aggregate_scenarios(scenarios::Array{Float64, 2}, probs::Array{Float64, 1},
                              mean::Array{Float64, 1}, cov::Array{Float64, 2},
@@ -13,12 +12,12 @@ function aggregate_scenarios(scenarios::Array{Float64, 2}, probs::Array{Float64,
     num_gen = size(cone_A, 2)
     new_scenarios = Array(Float64, dim, num_scen)
     new_probs = Array(Float64, num_scen)
-    new_num_scen = @eval @cpp ccall( ("aggregation_julia", $lib),
-                                    Int,
-                                    (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64},
-                                     Int, Int, Int,
-                                     Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Float64),
-                                    $new_scenarios, $new_probs, $scenarios, $probs, $dim, $num_scen, $num_gen,
-                                    $mean, $cov, $cone_A, $alpha)
+    new_num_scen = @cpp ccall( ("aggregation_julia", lib),
+                              Int,
+                              (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64},
+                               Int, Int, Int,
+                               Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Float64),
+                              new_scenarios, new_probs, scenarios, probs, dim, num_scen, num_gen,
+                              mean, cov, cone_A, alpha)
     return new_scenarios[:, 1:new_num_scen], new_probs[1:new_num_scen]
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
-using EllipseScenGen
+using EllipticalScenGen
+using Distributions
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Parameters
+mean = [0.1, 0.05]
+cov = [[1.0, -0.5] [-0.5, 1.5]]
+num_scen = 100
+beta = 0.95
+
+dim = length(mean)
+mv_norm = MvNormal(mean, cov)
+scenarios = rand(mv_norm, num_scen)
+probs = fill(1.0/num_scen, num_scen)
+alpha = quantile(Normal(), 0.95)
+cone_A = eye(dim)
+
+print("Running aggregate_scenarios with test arguments...")
+agg_scen, agg_prob = aggregate_scenarios(scenarios, probs, mean, cov, cone_A, alpha)
+print(agg_scen)
+print(agg_prob)
+print("\n")
