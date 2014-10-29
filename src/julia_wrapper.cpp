475c96ffeae4e2badc06b010d22da0af044eb986
Jamie Fairbrother <jfairbrother87@hotmail.com>
2014-10-29 09:17:19 +0000

    - Added some documentation to Julia wrapper for aggregation sampling - Added (commented out) prototype for a aggregation sampling wrapper function

--- src/julia_wrapper.cpp
+++ src/julia_wrapper.cpp
@@ -1,6 +1,21 @@
 #include "AggregationSampling.hpp"
 #include "julia_wrapper.hpp"
 
+/**
+ * Reduces scenario set by aggregating all scenarios in elliptical non-risk region.
+ *
+ * \param[out] new scenarios pointer to output scenarios (dim x num_scen preallocated double array)
+ * \param[out] new_probs pointer to output probabilities (num_scen preallocated double array)
+ * \param[in] old scenarios pointer to input scenarios
+ * \param[in] new_probs pointer to output probabilities
+ * \param dim dimension of problem
+ * \param num_scen number of scenarios to be generated
+ * \param num_gen number of generators for cone representing feasible region
+ * \param mean mean vector used for elliptical risk region
+ * \param cov covariance matrix used for elliptical risk region
+ * \param cone_A matrix whose rows are the cone generators of feasible region
+ * \param alpha cut-off distance for elliptical risk region
+ */
 int aggregation_julia(double *new_scenarios, double *new_probs, 
 		      double *old_scenarios, double *old_probs, 
 		      int dim, int num_scen, int num_gen,
@@ -21,3 +36,34 @@ int aggregation_julia(double *new_scenarios, double *new_probs,
   }
   return new_num_scen;
 }
+
+
+// /**
+//  * Aggregation sampling using specified sampler.
+//  * To use Julia callback for sampler, we use the "passthrough" idiom.
+//  *
+//  * \param[out] scenarios pointer to output scenarios (dim x num_scen preallocated double array)
+//  * \param[out] probs pointer to output probabilities (num_scen preallocated double array)
+//  * \param dim dimension of problem
+//  * \param num_scen number of scenarios to be generated
+//  * \param num_gen number of generators for cone representing feasible region
+//  * \param mean mean vector used for elliptical risk region
+//  * \param cov covariance matrix used for elliptical risk region
+//  * \param cone_A matrix whose rows are the cone generators of feasible region
+//  * \param alpha cut-off distance for elliptical risk region
+//  * \param thunk parameter used for Julia passthrough function
+//  * \param sampler sampling function (with pass-through parameter)
+//  */
+// int aggregation_sampling_julia(double *scenarios, double *probs,
+// 			       int dim, int num_scen, int num_gen,
+// 			       double *mean, double, *cov, double *cone_A,
+// 			       double alpha, void *thunk,
+// 			       void (*sampler)(void *thunk, double *scen))
+// {
+//   MeanCov meanCov(dim, mean, cov);
+//   AggregationSampling agg(meanCov, cone_A, num_gen, alpha);
+  
+//   ScenarioSet scenarios = agg(num_scen);
+  
+  
+// }
