b9f8aaa6c6b0b2dad8875dfc4760b112c75bf5f1
Jamie Fairbrother <jfairbrother87@hotmail.com>
2014-10-29 17:11:35 +0000

    - Added FiniteCone type and project function - Added test_FiniteCone.jl - Added appropriate exports for new Julia objects

--- src/EllipticalScenGen.jl
+++ src/EllipticalScenGen.jl
@@ -1,7 +1,9 @@
 module EllipticalScenGen
 
-export aggregate_scenarios
+export aggregate_scenarios, FiniteCone, lcp_solve, project
 
+include("LCP_julia.jl")
+include("FiniteCone.jl")
 include("ScenarioReduction.jl")
 
 end # module
