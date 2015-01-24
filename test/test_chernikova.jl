da24a791350b61e21a2b60e9af1383030713846c
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-01-24 15:42:11 +0000

    Added test to check that output of Chernikova is contained in the original polyhedral cone

--- test/test_chernikova.jl
+++ test/test_chernikova.jl
@@ -1,3 +1,5 @@
+using Distributions
+
 A = [[3.0 -4.0 1.0 0.0],
      [2.0 0.0 4.0 -1.0],
      [-4.0 -7.0 2.0 4.0],
@@ -5,3 +7,17 @@ A = [[3.0 -4.0 1.0 0.0],
      [6.0 -5.0 -4.0 2.0]]
 
 X = chernikova(A)
+
+# Function which checks anything generated from Chernikova output is in Polyhedral cone
+function test_polyhedral_contains_finite_cone(A::Matrix{Float64}, num_points::Int)
+    X = chernikova(A)
+    gens, dim = size(X)
+    dist = Uniform(0.0, 100.0)
+    for i in 1:num_points
+        λ = rand(dist, gens)
+        @test all(A*X'λ .>= 0.0)
+    end
+end
+
+
+test_polyhedral_contains_finite_cone(A, 10000)
