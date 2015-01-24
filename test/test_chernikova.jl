be5ee118a114434dd755ee92b7028e2c2a3f266e
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-01-24 18:08:34 +0000

    Added test to chernikova to test outputted cone from chernikova contains polyhedral cone (with positivity contraints)

--- test/test_chernikova.jl
+++ test/test_chernikova.jl
@@ -13,11 +13,29 @@ function test_polyhedral_contains_finite_cone(A::Matrix{Float64}, num_points::In
     X = chernikova(A)
     dim, gens = size(X)
     dist = Uniform(0.0, 100.0)
+    B = [A; eye(dim)]
     for i in 1:num_points
         λ = rand(dist, gens)
-        @test all(A*(X*λ) .>= 0.0)
+        @test all(B*(X*λ) .>= 0.0)
     end
 end
 
+# Function which checks that the polyhedral cone (intersected with the positive quadrant)
+# is contained in the finitely generated cone output by the Chernikova function.
+# This test is done by checking that the dual of the latter is contained in
+# the dual of the former.
+function test_finite_contains_polyhedral_cone(A::Matrix{Float64}, num_points::Int)
+    dim = size(A, 2)
+    B = [A; eye(dim)]  # Must add positivity constaints to original polyhedral cone
+    dual_gens= size(B,1)
+    X = chernikova(B)
+    dist = Uniform(0.0, 100.0)
+    for i in 1:num_points
+        λ = rand(dist, dual_gens)
+        @test all(X'B'λ .>= 0)
+    end
+end
 
+    
 test_polyhedral_contains_finite_cone(A, 10000)
+test_finite_contains_polyhedral_cone(A, 10000)
