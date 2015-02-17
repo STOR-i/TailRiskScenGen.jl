7e666eb6e70ea6a4a54ceb7bb29ae4ae5d8bf851
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-02-17 10:15:12 +0000

    Updated module includes and exports

--- src/EllipticalScenGen.jl
+++ src/EllipticalScenGen.jl
@@ -4,11 +4,11 @@ using Distributions
 
 import PDMats: dim
 
-export aggregate_scenarios, FiniteCone, chernikova, lcp_solve, project, RiskRegion, transformed_size, in_RiskRegion, aggregation_sampling, nonrisk_clustering, dim, cone_from_constraints, quota_cone
+export aggregate_scenarios, FiniteCone, PolyhedralCone, chernikova, lcp_solve, project, RiskRegion, transformed_size, in_RiskRegion, aggregation_sampling, nonrisk_clustering, dim, cone_from_constraints, quota_cone
 
 include("LCP_julia.jl")
 include("chernikova.jl")
-include("FiniteCone.jl")
+include("Cone.jl")
 include("RiskRegion.jl")
 include("nonrisk_clustering.jl")
 include("BruteForceRiskRegion.jl")
