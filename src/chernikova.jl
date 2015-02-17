c967cd02434226700075d6f15f3cde36e9e4332a
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-02-17 11:52:16 +0000

    Removed redundent functions from chernikova.jl

--- src/chernikova.jl
+++ src/chernikova.jl
@@ -209,33 +209,3 @@ function combine(mat::ChernMat{Int}, col::Int, i1::Int, i2::Int)
     return (t/mat.B[i1,col]) * mat.B[i1, :] - (t/mat.B[i2,col]) * mat.B[i2, :]
 end
 
-# Find the conical hull defined by the following constraints:
-#   1ᵀx = c
-#   Ax ≤ b
-#   x ≥ 0
-#  Returns FiniteCone object
-function cone_from_constraints(A::Matrix{Float64}, b::Vector{Float64}, c::Float64)
-    m, n = size(A)
-    m == length(b) || error("A and b must have consistent dimensions")
-    c > 0 || error("c must be strictly positive")
-    A_poly = Array(Float64, m+1, n)
-    A_poly[1,:] = ones(n)
-    for i in 1:m
-        A_poly[i+1,:] = (b[i]/c) * ones(n)' - A[i,:]
-    end
-    
-    return FiniteCone(chernikova(A_poly))
-end
-
-# Finds the conical hull of of the region define
-# by the following constraints:
-#
-# 1ᵀ x = 1
-# x ≤ u
-# x ≥ 0
-# Returns FiniteCone object
-function quota_cone(u::Vector{Float64})
-    n = length(u)
-    return cone_from_constraints(eye(n), u, 1.0)
-end
-
