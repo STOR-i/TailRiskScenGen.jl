72f81c0820ddbcdfdbc0da82b2065cb271afb588
Jamie Fairbrother <jfairbrother87@hotmail.com>
2017-01-12 16:38:43 +0000

    Added function for calculating probabilities of monotonic risk regions

--- src/monotonic_risk_regions.jl
+++ src/monotonic_risk_regions.jl
@@ -8,44 +8,44 @@ type MonotonicRiskRegion
     end
 end
 
-function add_to_risk_frontier!(Ω::MonotonicRiskRegion, x::VecF64)
+function add_to_risk_frontier!{T<:Real}(Ω::MonotonicRiskRegion, x::AbstractVector{T})
     l = Ω.risk_frontier
     if isa(l, Nil{VecF64})
-        return Ω.risk_frontier=list(x)
+        return Ω.risk_frontier=list(x[:])
     end
 
     while !isa(l, Nil{VecF64})
         pf = head(l)
         if vec_isless(x, pf)
             if isa(tail(l),Nil{VecF64})
-                return Ω.risk_frontier=list(x)
+                return Ω.risk_frontier=list(x[:])
             else
                 decapitate!(l)
             end
         end
         l = tail(l)
     end
-    return Ω.risk_frontier = cons(x, Ω.risk_frontier)
+    return Ω.risk_frontier = cons(x[:], Ω.risk_frontier)
 end
 
-function add_to_nonrisk_frontier!(Ω::MonotonicRiskRegion, x::VecF64)
+function add_to_nonrisk_frontier!{T<:Real}(Ω::MonotonicRiskRegion, x::AbstractVector{T})
     l = Ω.nonrisk_frontier
     if isa(l, Nil{VecF64})
-        return Ω.nonrisk_frontier=list(x)
+        return Ω.nonrisk_frontier=list(x[:])
     end
     
     while !isa(l, Nil{VecF64})
         pf = head(l)
         if vec_isless(pf, x)
             if isa(tail(l),Nil{VecF64})
-                return Ω.nonrisk_frontier=list(x)
+                return Ω.nonrisk_frontier=list(x[:])
             else
                 decapitate!(l)
             end
         end
         l = tail(l)
     end
-    return Ω.nonrisk_frontier = cons(x, Ω.nonrisk_frontier)
+    return Ω.nonrisk_frontier = cons(x[:], Ω.nonrisk_frontier)
 end
 
 function ∈(x::VecF64, Ω::MonotonicRiskRegion)
@@ -65,4 +65,57 @@ function ∈(x::VecF64, Ω::MonotonicRiskRegion)
             return true
         end
     end
+end
+
+function prob_nonrisk(dist::Distribution{Multivariate, Continuous}, β_list::Vector{Float64}, num_points_surv::Int = 50000, num_points_nonrisk::Int = 10000, α::Float64=0.99)
+    sample = rand(dist, num_points_nonrisk)
+    surv = SurvivorApproximator(rand(dist, num_points_surv), α)
+    Ω = Dict{Float64, MonotonicRiskRegion}()
+    probs = Dict{Float64,Float64}()
+    
+    for β in β_list
+        Ω[β] = MonotonicRiskRegion(surv, β)
+        probs[β] = 0.0
+    end
+
+    for i in 1:num_points_nonrisk
+        point = view(sample, :, i)
+        calc_surv_prob = false
+        surv_prob = 0.0
+        for β in β_list
+            if calc_surv_prob
+                probs[β] += (surv_prob > 1-β ? 1 : 0)
+                continue
+            else
+                shortcut = false
+                # Does sampled point dominate any point in risk region?
+                if below_nonrisk_frontier(point, Ω[β].nonrisk_frontier)
+                    shortcut=true
+                    probs[β] += 1
+                elseif above_risk_frontier(point, Ω[β].risk_frontier)
+                    shortcut=true
+                end
+
+                if !shortcut
+                    surv_prob, err = surv(point)
+                    if surv_prob > 1 - β
+                        probs[β]+=1
+                        if surv_prob - err > 1-β
+                            add_to_nonrisk_frontier!(Ω[β], point)
+                        end
+                    else
+                        if surv_prob + err < 1-β
+                            add_to_risk_frontier!(Ω[β], point)
+                        end
+                    end
+                    calc_surv_prob=true
+                end
+            end
+        end
+    end
+    
+    for β in β_list
+        probs[β]/= num_points_nonrisk
+    end
+    return probs
 end
--- test/test_monotonic_risk_region.jl
+++ test/test_monotonic_risk_region.jl
@@ -1,20 +1,33 @@
-using EllipticalScenGen: MonotonicRiskRegion, SurvivorApproximator
+using EllipticalScenGen: MonotonicRiskRegion, SurvivorApproximator, prob_nonrisk
 using Distributions
 using DataStructures
 using StatsFuns
 using Base: Test
 
+d = 2
 N = 10000
 α = 0.99
 β = 0.95
-dist = MvNormal(eye(2))
+dist = MvNormal(eye(d))
 sample = rand(dist, N)
 surv = SurvivorApproximator(sample, α)
 Ω = MonotonicRiskRegion(surv, 0.95)
 
+x = rand(dist)
+x ∈ Ω
+
+tic()
 count = 0
 for i in 1:1000
     x = rand(dist)
-    if x ∈ Ω count+=1 end
+    if !(x ∈ Ω) count+=1 end
 end
 approx_prob = count/1000
+toc()
+
+α = 0.99
+β_list = [0.95, 0.99]
+num_points_nonrisk = 1000
+num_points_surv = 10000
+dist = MvNormal(eye(d))
+prob_nonrisk(dist, β_list, num_points_surv, num_points_nonrisk, α)
